6ab2d01f5ae0d19b971e21ee44651de899a0aff3
Đoàn Ngọc Linh <linhdn.gen@gmail.com>
2026-03-17 18:11:51 +0700

    add switch categories navigation test

--- tests/discover.spec.ts
+++ tests/discover.spec.ts
@@ -10,3 +10,20 @@ test("default page load", async ({ page }) => {
   await expect(discoverPage.resultCards.first()).toBeVisible();
 });
 
+test.only("categories navigation", async ({ page }) => {
+  const discoverPage = new DiscoverPage(page);
+
+  await discoverPage.goto();
+
+  await discoverPage.selectCategory("Trend");
+  await expect(page).toHaveURL(/\/trend$/);
+  await expect(discoverPage.resultCards.first()).toBeVisible();
+
+  await discoverPage.selectCategory("Top rated");
+  await expect(page).toHaveURL(/\/top$/);
+  await expect(discoverPage.resultCards.first()).toBeVisible();
+
+  await discoverPage.selectCategory("Popular");
+  await expect(page).toHaveURL(/\/popular$/);
+  await expect(discoverPage.resultCards.first()).toBeVisible();
+});
